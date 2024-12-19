46cd7601dbc7e83ddb921becbda300da9045348e
AndrII <146057690+Andrew1231322@users.noreply.github.com>
2024-12-20 00:46:05 +0200

    Add files via upload

---
+++ my_sql.sql
@@ -0,0 +1,21 @@
+CREATE TABLE my_table (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    name VARCHAR(50)
+);
+
+
+ALTER TABLE my_table ADD COLUMN age INT;
+
+INSERT INTO my_table (name, age) VALUES 
+('A', 25),
+('B', 30),
+('C', 22),
+('D', 35);
+
+SELECT * FROM my_table;
+SELECT * FROM my_table WHERE age >= 25;
+
+CREATE PROCEDURE get_even_id_records()
+SELECT * FROM my_table WHERE id % 2 = 0;
+
+CALL get_even_id_records();
